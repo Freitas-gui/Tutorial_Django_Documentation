4989ec653a8e3ebe583b99f86d6b2ed0ba3d76d4
Freitas-gui <freitas-gui19@hotmail.com>
2020-03-14 12:20:53 -0300

    created view vote and results

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 app_name = 'polls'
 urlpatterns =[
-    path('<int:question_id>/',views.detail,name='detail'),
-    path('<int:question_id>/detaill',views.detaill,name='detaill'),
+    path('<int:question_id>/detail',views.detail,name='detail'),
     path('<int:question_id>/results',views.results,name='results'),
     path('<int:question_id>/vote/',views.vote, name='vote'),
     path('index/',views.index, name='index'),
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse , HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 
 def index(request):
@@ -10,23 +11,25 @@ def index(request):
     }
     return render(request,'polls/index.html', context)
 
-def detaill(request, question_id):
-    question = get_object_or_404(Question , pk=question_id)
-    return render(request, 'polls/detaill.html', {'question' : question})
-
 def detail(request, question_id):
-    this_question = Question.objects.filter(pk__in=[question_id])
-    context ={
-        'this_question' : this_question
-    }
-    return render(request,'polls/detail.html', context)
+    question = get_object_or_404(Question , pk=question_id)
+    return render(request, 'polls/detail.html', {'question' : question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question' : question})
 
 def vote(request, question_id):
-    return HttpResponse("You're votting on question %s."%question_id)
-
-
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question' : question,
+            'error_message' : "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id))) #boa pratica redirecionar ao inves de somente encaminhar
 # Create your views here.
